6ab8503e25f386528c999c71e6686abeda2e8b22
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2017-04-16 15:37:58 -0400

    Implement author update success path

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -27,6 +27,21 @@ class AuthorsController < ApplicationController
 
   end
 
+
+  def edit
+    @author = Author.find(params[:id]) 
+  end
+
+  def update 
+    @author = Author.find(params[:id]) 
+    if @author.update(author_params)
+    flash[:success] = "Author has been updated"
+    redirect_to @author
+    end
+  end
+
+
+
   private 
   def author_params
     params.require(:author).permit(:first_name, :last_name)
