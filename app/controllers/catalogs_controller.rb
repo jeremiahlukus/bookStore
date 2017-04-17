12c09969aa9bc3438c1a59ee855d83d9028a9608
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2017-04-17 16:19:43 -0400

    Created author

---
+++ app/controllers/catalogs_controller.rb
@@ -0,0 +1,9 @@
+class CatalogsController < ApplicationController 
+
+  def index
+
+
+  end
+
+
+end
---
+++ spec/features/author_feature_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+
+RSpec.feature "Creating Authors" do 
+
+  scenario "with valid inputs succeeds" do
+    visit root_path
+
+    click_link "Authors"
+    click_link "Add new author"
+
+    fill_in "First name", with: "Sue"
+    fill_in "Last name", with: "Doe"
+
+    click_button "Create Author"
+    #Flash message
+    expect(page).to have_content("Author has been created")
+  end
+  
+
+end
+
